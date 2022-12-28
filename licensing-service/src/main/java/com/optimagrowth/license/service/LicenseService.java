fb3efb80f317e467f4d9e65a868bfbdc5cf5bd49
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-12-29 00:55:35 +0530

    Add the gateway server for consolidating cross cutting concerns

--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -7,14 +7,17 @@ import com.optimagrowth.license.repository.LicenseRepository;
 import com.optimagrowth.license.service.client.OrganizationDiscoveryClient;
 import com.optimagrowth.license.service.client.OrganizationFeignClient;
 import com.optimagrowth.license.service.client.OrganizationRestTemplateClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeoutException;
@@ -23,51 +26,51 @@ import java.util.concurrent.TimeoutException;
 @Service
 public class LicenseService {
 
-	@Autowired
-	MessageSource messages;
+    @Autowired
+    MessageSource messages;
 
-	@Autowired
-	private LicenseRepository licenseRepository;
+    @Autowired
+    private LicenseRepository licenseRepository;
 
-	@Autowired
-	ServiceConfig config;
+    @Autowired
+    ServiceConfig config;
 
-	@Autowired
-	OrganizationFeignClient organizationFeignClient;
+    @Autowired
+    OrganizationFeignClient organizationFeignClient;
 
-	@Autowired
-	OrganizationRestTemplateClient organizationRestClient;
+    @Autowired
+    OrganizationRestTemplateClient organizationRestClient;
 
-	@Autowired
-	OrganizationDiscoveryClient organizationDiscoveryClient;
+    @Autowired
+    OrganizationDiscoveryClient organizationDiscoveryClient;
 
-	@Autowired
-	CircuitBreakerRegistry registry;
+    @Autowired
+    CircuitBreakerRegistry registry;
 
 
-	private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
+    private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
 
-	public License getLicense(String licenseId, String organizationId, String clientType){
-		License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
-		if (null == license) {
-			throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null),licenseId, organizationId));	
-		}
+    public License getLicense(String licenseId, String organizationId, String clientType) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (null == license) {
+            throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null), licenseId, organizationId));
+        }
 
-		Organization organization = retrieveOrganizationInfo(organizationId, clientType);
-		if (null != organization) {
-			license.setOrganizationName(organization.getName());
-			license.setContactName(organization.getContactName());
-			license.setContactEmail(organization.getContactEmail());
-			license.setContactPhone(organization.getContactPhone());
-		}
+        Organization organization = retrieveOrganizationInfo(organizationId, clientType);
+        if (null != organization) {
+            license.setOrganizationName(organization.getName());
+            license.setContactName(organization.getContactName());
+            license.setContactEmail(organization.getContactEmail());
+            license.setContactPhone(organization.getContactPhone());
+        }
 
-		return license.withComment(config.getProperty());
-	}
+        return license.withComment(config.getProperty());
+    }
 
-	@CircuitBreaker(name = "organizationService")
-	private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
-		Organization organization = null;
-		organization = organizationRestClient.getOrganization(organizationId);
+    @CircuitBreaker(name = "organizationService")
+    private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
+        Organization organization = null;
+        organization = organizationRestClient.getOrganization(organizationId);
 
 //		switch (clientType) {
 //		case "feign":
@@ -86,57 +89,72 @@ public class LicenseService {
 //			organization = organizationRestClient.getOrganization(organizationId);
 //		}
 //
-		return organization;
-	}
+        return organization;
+    }
 
-	public License createLicense(License license){
-		license.setLicenseId(UUID.randomUUID().toString());
-		licenseRepository.save(license);
+    public License createLicense(License license) {
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
 
-		return license.withComment(config.getProperty());
-	}
+        return license.withComment(config.getProperty());
+    }
 
-	public License updateLicense(License license){
-		licenseRepository.save(license);
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
 
-		return license.withComment(config.getProperty());
-	}
+        return license.withComment(config.getProperty());
+    }
 
-	public String deleteLicense(String licenseId){
-		String responseMessage = null;
-		License license = new License();
-		license.setLicenseId(licenseId);
-		licenseRepository.delete(license);
-		responseMessage = String.format(messages.getMessage("license.delete.message", null, null),licenseId);
-		return responseMessage;
+    public String deleteLicense(String licenseId) {
+        String responseMessage = null;
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+        responseMessage = String.format(messages.getMessage("license.delete.message", null, null), licenseId);
+        return responseMessage;
 
-	}
+    }
 
-	@CircuitBreaker(name = "licenseService")
-	public List<License> getLicensesByOrganization(String organizationId) throws TimeoutException {
-		logger.debug("**********************");
-		System.out.println("***************************************");
-		System.out.println(registry.circuitBreaker("licenseService").getState());
-		System.out.println(registry.circuitBreaker("licenseService").getMetrics().getFailureRate());
-		System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfFailedCalls());
-		System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfSuccessfulCalls());
-		System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfBufferedCalls());
-		System.out.println("**********************");
-		randomlyRunLong();
-		return licenseRepository.findByOrganizationId(organizationId);
-	}
+    @CircuitBreaker(name = "licenseService", fallbackMethod = "buildFallBackLicenseList")
+    @Retry(name = "retryLicenseService", fallbackMethod = "buildFallbackLicenseList")
+    @Bulkhead(name= "bulkheadLicenseService", type = Bulkhead.Type.THREADPOOL, fallbackMethod= "buildFallbackLicenseList")
+    public List<License> getLicensesByOrganization(String organizationId) throws TimeoutException {
+        logger.debug("**********************");
+        System.out.println("***************************************");
+        System.out.println(registry.circuitBreaker("licenseService").getState());
+        System.out.println(registry.circuitBreaker("licenseService").getMetrics().getFailureRate());
+        System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfFailedCalls());
+        System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfSuccessfulCalls());
+        System.out.println(registry.circuitBreaker("licenseService").getMetrics().getNumberOfBufferedCalls());
+        System.out.println("**********************");
+        randomlyRunLong();
+        return licenseRepository.findByOrganizationId(organizationId);
+    }
 
-	private void randomlyRunLong() throws TimeoutException{
+    public List<License> buildFallBackLicenseList(String organizationId, Throwable t) {
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License();
+        license.setLicenseId("0000000-00-00000");
+        license.setOrganizationId(organizationId);
+        license.setProductName(
+                "Sorry no licensing information currently available");
+        fallbackList.add(license);
+        return fallbackList;
+    }
+
+
+    private void randomlyRunLong() throws TimeoutException {
 //		Random rand = new Random();
 //		int randomNum = rand.nextInt(2) + 1;
-		sleep();
-	}
-	private void sleep() throws TimeoutException {
-		try {
-			Thread.sleep(1000);
-			throw new java.util.concurrent.TimeoutException();
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage());
-		}
-	}
+        sleep();
+    }
+
+    private void sleep() throws TimeoutException {
+        try {
+            Thread.sleep(1000);
+            throw new java.util.concurrent.TimeoutException();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+    }
 }
